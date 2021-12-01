d5b4cf7551a3b2d4d86564abc3286c2dd7b30fb3
Jason <jimanproductions@gmail.com>
2021-12-01 11:55:33 +0100

    cleaned js code

--- app.js
+++ app.js
@@ -27,26 +27,20 @@ let missions = [
         "challenge": "Eat 2 fruits today."
     },
     {
+        "type": "nutrition",
+        "name": "research",
+        "challenge": "Do some online search about the benefits of healthy eating."
+    },
+    {
         "type": "social",
         "name": "talk",
         "challenge": "Call 2 friends."
     }
 ]
 
-let pushups = () => {
-    let max = 100;
-    return "Do " + max + " pushups today."
-}
-
-let walk = () => {
-    let max = 60;
-    return "Walk " + max + " minutes today."
-}
-
 let getTask = () => {
     max = missions.length;
     let num = Math.floor(Math.random() * max);
     console.log(num)
     document.getElementById("task").innerHTML = missions[num].challenge;
-    // return "Do 30 minutes of yoga!"
 }
